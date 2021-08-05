d4e20b1e4f2c02bb6952b087d23782cb3396cf11
arunpratapsingh17 <218apsj@gmail.com>
2021-08-05 11:31:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import axios from 'axios';
+import MediaCard from './components/MediaCard';
+import LoadingComp from "./components/LoadingComp";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [input,setInput] = useState("");
+  const [loading,setLoading] = useState(false);
+  const [res,setRes] = useState([])
+  const handleChange=(event)=>{
+    setInput(event.target.value)
+  }
+  const handleSubmit=async()=>{
+    setLoading(true);
+    console.log("Clicked");
+    //API call
+    var result = await axios.get(`https://api.jikan.moe/v3/search/anime?q=${input}&amp;limit=16`);
+    console.log(result.data.results);
+    setRes(result.data.results);
+    setLoading(false);
+  }
+  console.log(res);
+  return (loading)?(<LoadingComp  />):(
+    //Search Bar
+    (<div className="App">
+      <div className="inputBox">
+        <input className="input" value={input} onChange={handleChange}>
+          </input>
+          <button className="button" onClick={handleSubmit}>
+             Go
+        </button>
+      </div>
+      <div>
+          <p className="apiname">
+            Requesting:{`https://api.jikan.moe/v3/search/anime?q=${input}&amp;limit=16`}
+          </p>
+        </div>
+      <div className="list">
+        {
+          res.map((obj)=>{
+             var props = {title:obj.title,image:obj.image_url};
+            //console.log(props);
+            return <MediaCard key={obj.mal_id} props={props}/>
+          })
+        }
+      </div>
     </div>
-  );
+  ));
 }
 
 export default App;
